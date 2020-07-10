ebbe992ce4e521e347b98955a86d69e6e102bd1e
AcademyGaffney <michaelkgaffney@gmail.com>
2020-07-10 07:35:30 -0500

    Session 3

---
+++ code/JavaBasicStuff/src/com/academy/javabasics/session2/Casting.java
@@ -0,0 +1,15 @@
+package com.academy.javabasics.session2;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		System.out.println((byte)1152);
+		System.out.println((int)43.9);
+		System.out.println((int)1.352e200);
+		System.out.println((char)43);
+		System.out.println((char)65579);
+		
+
+	}
+
+}
---
+++ code/JavaBasicStuff/src/com/academy/javabasics/session3/Sandbox.java
@@ -0,0 +1,10 @@
+package com.academy.javabasics.session3;
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
