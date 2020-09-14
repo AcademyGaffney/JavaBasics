53fb842a50a3b127cef5c6e4890c08a8e0141e89
AcademyGaffney <michaelkgaffney@gmail.com>
2020-09-14 08:55:20 -0500

    day 1

--- code/JavaBasics/src/com/academy/javabasics/session2/APIStuff.java
+++
@@ -1,9 +0,0 @@
-package com.academy.javabasics.session2;
-
-public class APIStuff {
-
-	public static void main(String[] args) {
-		System.out.println(Math.pow(2, 31) - 1);
-	}
-
-}
--- code/JavaBasics/src/com/academy/javabasics/session2/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.academy.javabasics.session2;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
